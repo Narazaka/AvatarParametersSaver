110aa473d4a81d9c2807cf911ab886c68333ccf4
Narazaka <info@narazaka.net>
2024-02-11 19:27:23 +0900

    IEditorOnly

--- Scripts/AvatarParametersPresets.cs
+++ Scripts/AvatarParametersPresets.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using VRC.SDKBase;
 
 namespace net.narazaka.vrchat.avatar_parameters_saver
 {
-    public class AvatarParametersPresets : MonoBehaviour
+    public class AvatarParametersPresets : MonoBehaviour, IEditorOnly
     {
         public AvatarParametersSaverPresetGroup AvatarParametersSaverPresetGroup = new AvatarParametersSaverPresetGroup();
     }
